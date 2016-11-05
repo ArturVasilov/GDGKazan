6957b5d3beec7296ee4935da50b497ba7ad5a021
Artur Vasilov <artur.vasilov@dz.ru>
2016-11-05 19:00:10 +0300

    added okhttp3 downloader for picasso

--- app/src/main/java/ru/gdg/kazan/gdgkazan/GDGKazanApp.java
+++ app/src/main/java/ru/gdg/kazan/gdgkazan/GDGKazanApp.java
@@ -5,10 +5,17 @@ import android.app.Application;
 import android.content.Context;
 import android.support.annotation.NonNull;
 
+import com.jakewharton.picasso.OkHttp3Downloader;
+import com.squareup.picasso.Downloader;
+import com.squareup.picasso.Picasso;
+
 /**
  * @author Artur Vasilov
  */
 public class GDGKazanApp extends Application {
+
+    private static final int MEGABYTES_IN_BYTES = 1024 * 1024;
+    private static final int DEFAULT_MEGABYTES_COUNT = 50;
 
     @SuppressLint("StaticFieldLeak")
     private static Context sAppContext;
@@ -17,6 +24,15 @@ public class GDGKazanApp extends Application {
     public void onCreate() {
         super.onCreate();
         sAppContext = this;
+
+        int megabytes = DEFAULT_MEGABYTES_COUNT * MEGABYTES_IN_BYTES; //50 megabytes
+        Downloader downloader = new OkHttp3Downloader(this, megabytes);
+        Picasso picasso = new Picasso.Builder(this).downloader(downloader).build();
+        try {
+            Picasso.setSingletonInstance(picasso);
+        } catch (Exception ignored) {
+            //Picasso instance was already set
+        }
     }
 
     @NonNull
